99eaf7547e28167f8832a353ea0a8394d536a4df
EasiFyre <82217777+EasiFyre@users.noreply.github.com>
2021-06-02 12:09:30 +0530

    Create Variables.js

---
+++ ReactComponents/Variables.js
@@ -0,0 +1,28 @@
+let AllCards = {
+    UpVariables : {
+        MovieName: "UP",
+        CardButtonColor: "#2895cb",
+        MovieType: "Family",
+        MovieYear: "2009",
+        MovieAgeRating: "G",
+        MoviePoster: "https://i.pinimg.com/originals/32/01/1b/32011b37ea9df5d484ba2e82c3926f87.jpg",
+    },
+    BlackPantherVariables : {
+        MovieName: "Black Panther",
+        CardButtonColor: "#664eae",
+        MovieType: "Action",
+        MovieYear: "2018",
+        MovieAgeRating: "12+",
+        MoviePoster: "https://i.pinimg.com/originals/49/c1/6b/49c16b29a02235ee7accc6135ca9ca6e.jpg",
+    },
+    ToyStory4Variables : {
+        MovieName: "Toy Story 4",
+        CardButtonColor: "#f1060b",
+        MovieType: "Family",
+        MovieYear: "2019",
+        MovieAgeRating: "G",
+        MoviePoster: "https://i.pinimg.com/originals/aa/9e/08/aa9e08d5da36961a492f3e84bda3a5b7.jpg",
+    },
+}
+
+export default AllCards;
