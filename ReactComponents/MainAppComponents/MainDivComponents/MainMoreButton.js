f1664f7abc8575d2fc8681f4b54312fc16d18357
EasiFyre <82217777+EasiFyre@users.noreply.github.com>
2021-06-02 12:20:05 +0530

    Update MainMoreButton.js

--- ReactComponents/MainAppComponents/MainDivComponents/MainMoreButton.js
+++ ReactComponents/MainAppComponents/MainDivComponents/MainMoreButton.js
@@ -1,5 +1,5 @@
 // To Make Javascript Accept JSX (JavaScript HTML)
-import React from "react";
+import React from "https://raw.githubusercontent.com/EasiFyre/easifyre.github.io/main/Modules/react.js";
 
 // The Component
 const MainMoreButton = (props) => {
