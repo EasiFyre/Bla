1ac44e8b06e33c201d064fa807dc12d6ac292330
EasiFyre <82217777+EasiFyre@users.noreply.github.com>
2021-06-02 12:10:11 +0530

    Create MainDiv.js

---
+++ ReactComponents/MainAppComponents/MainDiv.js
@@ -0,0 +1,40 @@
+// To Make Javascript Accept JSX (JavaScript HTML)
+import React from "react"
+
+// To Render Each Component In This Component
+import MovieName from "./MainDivComponents/MovieName.js"
+import MovieSpecifics from "./MainDivComponents/MovieSpecifics.js"
+import MainMoreButton from "./MainDivComponents/MainMoreButton.js"
+import MainWatchButton from "./MainDivComponents/MainWatchButton.js"
+import Poster from "./MainDivComponents/Poster.js"
+
+
+// The Specificatations Of The Cards
+import CardVariables from "./Variables"
+
+// The Poster's Location
+let ImgSrc = CardVariables.MoviePoster;
+
+// The Component
+const MainDiv = (props) => {
+    return(
+        <div className="GreyDiv">
+            <Poster Content= {props.CardVariable} />
+            <div className="InnerDiv">
+                <MainWatchButton Content= {props.CardVariable} />
+                <MainMoreButton Content= {props.CardVariable} />
+                <MovieName Content= {props.CardVariable} />
+                <MovieSpecifics Content= {props.CardVariable} />
+                {/*
+                <MovieName Content= {props.CardVariable} />
+                <MovieSpecifics Content= {props.CardVariable} />
+                <MainMoreButton Content= {props.CardVariable} />
+                <MainWatchButton Content= {props.CardVariable} />
+                */}
+            </div>
+        </div>
+    )
+}
+
+// Exporting The Component
+export default MainDiv;
