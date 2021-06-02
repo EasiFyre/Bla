8cf3f729469f31c79f4f7155ac2d948e81a873a3
EasiFyre <82217777+EasiFyre@users.noreply.github.com>
2021-06-02 12:09:03 +0530

    Create MainApp.js

---
+++ ReactComponents/MainApp.js
@@ -0,0 +1,23 @@
+// To Make Javascript Accept JSX (JavaScript HTML)
+import React from "react"
+
+import AllCards from "./Variables"
+
+// To Render Each Component In This Component
+import MainDiv from "./MainAppComponents/MainDiv"
+import MainClock from "./MainAppComponents/OtherComponents/MainClock"
+
+// The Component
+const MainApp = () => {
+    return(
+        <div>
+            <MainDiv CardVariable={AllCards.UpVariables} />
+            <MainDiv CardVariable={AllCards.BlackPantherVariables} />
+            <MainDiv CardVariable={AllCards.ToyStory4Variables} />
+            <MainClock />
+        </div>
+    )
+}
+
+// Exporting The Component
+export default MainApp
